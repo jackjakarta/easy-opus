c32cbcdf21e49ca309338d9072d837f3cd960f11
Alex Termure <al.termure@gmail.com>
2024-05-14 11:43:15 +0200

    user auth and register implemented with clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,21 @@
+import { ClerkProvider } from '@clerk/nextjs';
 import { NavLinks } from "./ui/nav-links";
 import './globals.css';
 
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>
-        <NavLinks />
-        <main className="my-8 mx-auto w-full max-w-2xl">{children}</main>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          <header>
+            <NavLinks />  
+          </header>
+          <main className="my-8 mx-auto w-full max-w-2xl">
+            {children}
+          </main>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 };
--- src/app/ui/nav-links.tsx
+++ src/app/ui/nav-links.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 
@@ -23,7 +24,28 @@ export function NavLinks() {
             >
                 Events
             </Link>
+            
+            <SignedIn>
+                <UserButton />
+            </SignedIn>
+            
+            <SignedOut>
+                <Link 
+                    className={`px-4 py-2 rounded-lg font-medium transition-colors duration-300 
+                                ${pathname === '/sign-in' ? 'bg-indigo-600 text-white' : 'text-indigo-500 hover:bg-indigo-100'}`} 
+                    href='/sign-in'
+                >
+                    Sign In
+                </Link>
+
+                <Link 
+                    className={`px-4 py-2 rounded-lg font-medium transition-colors duration-300 
+                                ${pathname === '/sign-up' ? 'bg-indigo-600 text-white' : 'text-indigo-500 hover:bg-indigo-100'}`} 
+                    href='/sign-up'
+                >
+                    Sign Up
+                </Link>
+            </SignedOut>  
         </nav>
     );
 };
-
